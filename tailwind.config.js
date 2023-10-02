99227def04b41af159460a09759bcd1e12649bf4
oleg sukharev <olegsukharev1991@gmail.com>
2023-10-02 16:08:51 +0200

    global settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#55A2EE",
+        "secondary": "#FEDBDB",
+      }
+    },
   },
   plugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import * as path from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  build: {},
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  base: "/",
 });
