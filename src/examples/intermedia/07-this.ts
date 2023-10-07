69299d084105ac5c7e8dc04bae2087013500e4fa
oleg sukharev <olegsukharev1991@gmail.com>
2023-10-07 16:53:24 +0200

    intermedia examples

---
+++ src/examples/intermedia/07-this.ts
@@ -0,0 +1,14 @@
+// js example
+
+export const user = {
+  id: 123,
+  admin: false,
+  becomeAdmin: function () {
+    this.admin = true;
+  },
+};
+
+// <button onClick="myClickHandler">Click Me!</button>
+function myClickHandler(this: HTMLButtonElement, event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+  this.disabled = true;
+}
