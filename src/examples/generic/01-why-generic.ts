124a78c778c1ce280bd4eeb00858306e36aac861
oleg sukharev <olegsukharev1991@gmail.com>
2023-10-09 18:41:02 +0200

    generic base

---
+++ src/examples/generic/01-why-generic.ts
@@ -0,0 +1,14 @@
+function head(value: string): string;
+function head(value: number[]): number;
+function head(value: string[]): string;
+function head(value: boolean[]): boolean;
+function head(value: Date[]): Date;
+function head(value: any): any {
+  return value[0];
+}
+
+function headCommon<Type>(arg: Type): Type {
+  return arg;
+}
+
+export {};
