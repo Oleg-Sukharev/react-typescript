c729118edb6c230d96873e830d98b513481fc369
oleg sukharev <olegsukharev1991@gmail.com>
2024-01-15 22:06:23 +0100

    input and textarea

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -2,20 +2,32 @@ import * as React from "react";
 
 import mc from "@/utils/mergeClasses";
 
-export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  dirtyField?: boolean;
+  error?: string | any;
+  label: string;
+};
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ className, ...props }, ref) => {
-  return (
-    <textarea
-      className={mc(
-        "flex min-h-[80px] w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
-Textarea.displayName = "Textarea";
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, error, label, dirtyField, ...props }, ref) => {
+    return (
+      <div className={className}>
+        <label htmlFor={label} className={mc("text-sm ", dirtyField && "text-blue-400")}>
+          {label}
+        </label>
+        <textarea
+          id={label}
+          className={mc(
+            "placeholder:gray-700 block w-full rounded-md border p-2 py-1.5 pr-10 text-gray-700 focus:border-blue-300",
+            dirtyField && "text-blue-400",
+          )}
+          ref={ref}
+          {...props}
+        />
+        {error && <span className="mt-2 text-sm text-red-600">{error}</span>}
+      </div>
+    );
+  },
+);
 
-export { Textarea };
+export default Textarea;
